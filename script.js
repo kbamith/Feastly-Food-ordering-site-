05b6d83abb3735e6b47c304bf1018bcb779a39d9
kbamith <amithkb816@gmail.com>
2025-03-02 12:28:21 +0530

    Episode 1

---
+++ script.js
@@ -0,0 +1,12 @@
+const parent=React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"I am an h1 tag") ,React.createElement("h2",{},"I am an h1 tag")]    
+)],  [React.createElement("div",{id:"child2"},
+    [React.createElement("h1",{},"I am an h1 tag") ,React.createElement("h2",{},"I am an h1 tag")]    
+)]
+)
+
+
+
+ const root=ReactDOM.createRoot(document.getElementById("output"));
+ root.render(parent)
